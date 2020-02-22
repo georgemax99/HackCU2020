fd19a49ea5056ca61ee014b663dff03ccd153569
George Young <gmyoungwps@gmail.com>
2020-02-22 15:11:01 -0700

    made firstviewcontroller

---
+++ DroPin/DroPin/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  DroPin
+//
+//  Created by Max on 2/22/20.
+//  Copyright © 2020 Max. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+
+    var phoneNumberField : UITextField!
+    var submitButton : UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        initUI()
+    }
+
+    func initUI() {
+        phoneNumberField = UITextField(frame: CGRect(x: 100, y: 500, width: 200, height: 100))
+        phoneNumberField.placeholder = "Phone number"
+        phoneNumberField.textColor = .black
+        self.view.addSubview(phoneNumberField)
+        
+        submitButton = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 100))
+        submitButton.setTitle("login", for: .normal)
+        submitButton.setTitleColor(.black, for: .normal)
+        submitButton.addTarget(self, action: #selector(loginAction), for: .touchUpInside)
+        self.view.addSubview(submitButton)
+        
+        self.view.backgroundColor = .white
+    }
+}
--- DroPin/DroPin/ViewController.swift
+++ DroPin/DroPin/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var loginButton : UIButton!
+    var signupButton : UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        initUI()
     }
-
-
+    
+    func initUI() {
+        loginButton = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 100))
+        loginButton.setTitle("login", for: .normal)
+        loginButton.setTitleColor(.black, for: .normal)
+        loginButton.addTarget(self, action: #selector(loginAction), for: .touchUpInside)
+        self.view.addSubview(loginButton)
+        
+        signupButton = UIButton(frame: CGRect(x: 100, y: 300, width: 200, height: 100))
+        signupButton.setTitle("sign up", for: .normal)
+        signupButton.setTitleColor(.black, for: .normal)
+        signupButton.addTarget(self, action: #selector(signupAction), for: .touchUpInside)
+        self.view.addSubview(signupButton)
+        
+        self.view.backgroundColor = .white
+    }
+    
+    @objc func loginAction() {
+        
+    }
+    
+    @objc func signupAction() {
+        
+    }
 }
 
