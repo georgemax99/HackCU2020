64873e3c324bcebc84f4090f9e795ddd1999b41e
George Young <gmyoungwps@gmail.com>
2020-02-22 12:47:25 -0700

    first commit

---
+++ DroPin/DroPinTests/DroPinTests.swift
@@ -0,0 +1,34 @@
+//
+//  DroPinTests.swift
+//  DroPinTests
+//
+//  Created by Max on 2/22/20.
+//  Copyright © 2020 Max. All rights reserved.
+//
+
+import XCTest
+@testable import DroPin
+
+class DroPinTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
